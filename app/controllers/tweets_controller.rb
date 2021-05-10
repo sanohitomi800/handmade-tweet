c541759273191689025d62dc70ff0ebb84a84e7a
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-05-10 10:19:25 +0900

    編集機能実装完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,16 +5,27 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+  
   def create
     Tweet.create(tweet_params)
   end
-def destroy
-  tweet = Tweet.find(params[:id])
-  tweet.destroy
-end
-
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+  
   private
-def tweet_params
-  params.repuire(:tweet).permit(:name, :image, :text)
-end
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
